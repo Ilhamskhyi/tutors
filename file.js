eeb9247792f5965eca9a1e0d624c6394510d0fdb
Ilhamskhyi <98175032+Ilhamskhyi@users.noreply.github.com>
2022-01-25 23:05:13 +0700

    Add files via upload

---
+++ file.js
@@ -0,0 +1,98 @@
+Asllamualikum wr.wb 
+
+SALAM KENAL PRIK• TERUTAMA PARA 'MASTAH','AND', 'CREATOR./Whatsapp-BOT',
+SAYA OWNER CATZY-BOT MULTI DEVICE
+
+
+KALI INI IM RECODE BASE BOT MULTI DEVICE BASE ORI NYA "HISOKA-MOROU",
+'DON'T HILAP./POHO./LUPA', SUBSCRIBE GUYS BIAR KEDEPANYA TAMBAH SEMANGAT NGONTEN
+
+TANKS TO
+
+ALLAH SWT
+UMI 
+ABAH
+ILHAM - SKHYZI
+FRM DEVELOPER
+PENYEDIA API's
+ALL CREATOR WAHTSAP BOT
+
+BAHAN - BAHAN 
+
+niat dan ke yakinan
+apk zachiver download di playstore
+apk termux download di playstore
+apk MT manager / pakai EditQuick
+dan 2 Hanphone Untuk scan 
+
+JANGAN LUPA DI SAAT MAU SCAN AKTIFAKN DULU DI WhatsApp web versi beta / bergabung
+
+GITHUB SC ORI HISOKA-MOROU
+
+https://github.com/DikaArdnt/Hisoka-Morou
+
+GITHUB OWNER CATZY-BOT MD
+
+https://github.co./IlhamSkhyzi/catzy
+
+GROUP WHATSAP BOT
+
+https://chat.whatsapp.com/Hw1iYmAq8Cp2Ju64T7el09
+
+NUMBER OWNER
+
+https://wa.me/+687710621
+
+SCURE CODE sebelum di gunakan : delete session nya!
+
+https://www.mediafire.com/file/olqwlx8j1fbpxhx/CATZY-BOT+MD.zip/file
+
+node_modules ada di deskripsi group whatsap bot
+no password
+
+password tanyakan ke owner
+syarat ketentuan screenshot bukti subscribers
+send ke pesan chat......../>¹
+
+Run Yang Sering Gua Pakai
+
+heroku.com
+railway.com
+replit.com
+
+commander termux
+
+→ pkg update && pkg upgrade
+→ pkg install git
+→ pkg install libweb
+→ pkg install imagemagick
+→ pkg install nodejs
+→ termux-setup-storage
+→ cd /sdcard
+→ cd nama file
+→ npm install
+→ npm start
+
+clear tinggal scan code QR nya
+
+jika eror atau kalian lupa hapus sesi ↓
+
+CRTL → klik <c> lalu enter
+
+kalian bisa hapus sesion dulu atau
+
+$ rm -rf catzy bot-md
+$ npm start
+
+JAN LUPA DONASI NYA BUAT BELI API's SAMA BUAT BELI KOPI
+
+𝘿𝙊𝙉𝘼𝙎𝙄 𝙑𝙄𝘼 •
+
+➥ 𝙂𝙊𝙋𝘼𝙔
+➥ 𝙊𝙑𝙊
+➥ 𝘿𝘼𝙉𝘼
+➥ 𝙋𝘼𝙔𝙋𝘼𝙇
+➥ 𝘽𝘼𝙉𝙆
+➥ 𝙋𝙃𝙊𝙉𝙀𝙉𝙐𝙈𝘽𝙀𝙍
+
+https://wa.me/+6287773710155
